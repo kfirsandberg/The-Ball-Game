3e4c0f070fbdca52fb8894da47eff7afbafe3acf
kfirsandberg <kfirsan07@gmail.com>
2024-09-10 18:03:53 +0300

    adding onBallClick function to work

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function onBallClick(){
-    console.log("hey you")
+    var element =document.querySelector("div")
+    var currentTesxt = Number(element.innerText)
+    element.style.width = currentTesxt+ 50+'px'
+    element.style.height = currentTesxt+ 50+'px'
+    element.innerText = currentTesxt +50
 }
